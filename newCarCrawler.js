b04c52232a2b50da849405bf24a5c6a4fbdebb60
Jiheon Lee <wlgjsdl0@gmail.com>
2021-05-17 09:24:56 +0900

    Modify HN Refactoring

--- newCarCrawler.js
+++ newCarCrawler.js
@@ -1,9 +1,8 @@
-// 먼저 퇴근해서 죄송합니다 ...
+// HN Refactoring
 
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 // const fs = require('fs');
-
 
 function rgbToHex(rgb) {
   if (rgb.search("rgb") == -1) {
@@ -19,7 +18,44 @@ function rgbToHex(rgb) {
 
 // const timer = (ms) => new Promise(res => setTimeout(res, ms));
 
-car_brand_no = ['303', '304', '307', '312', '326', '321', '362', '349', '371', '376', '413', '422', '618', '367', '459', '394', '399', '381', '440', '436', '445', '390', '385', '491', '486', '514', '500', '569', '573', '546', '587']
+// 크롤링 기준, 순서
+// brand_no => model num => 색상
+
+car_brand_no = [
+  "303",
+  "304",
+  "307",
+  "312",
+  "326",
+  "321",
+  "362",
+  "349",
+  "371",
+  "376",
+  "413",
+  "422",
+  "618",
+  "367",
+  "459",
+  "394",
+  "399",
+  "381",
+  "440",
+  "436",
+  "445",
+  "390",
+  "385",
+  "491",
+  "486",
+  "514",
+  "500",
+  "569",
+  "573",
+  "546",
+  "587",
+];
+
+console.log(car_brand_no.length);
 
 // 514 page 많음 예외 처리 필요
 
@@ -32,125 +68,115 @@ let url, content, $, lists;
     // 옵션으로 headless 사용
     const browser = await puppeteer.launch({
       // executablePath: '/usr/bin/chromium-browser',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
       headless: false,
       // 속도 지정 m/s
-      slowMo: 50
+      slowMo: 50,
     });
 
     // 새로운 페이지를 열음
     const page = await browser.newPage();
-    console.log('Open browser.');
+    console.log("Open browser.");
 
     // 페이지의 크기를 설정
     await page.setViewport({
       width: 1366,
-      height: 768
+      height: 768,
     });
 
-    // 다운로드 파일 경로 지정
-    // await page._client.send('Page.setDownloadBehavior', {
-    //   behavior: 'allow',
-    //   downloadPath: './public/excels/'
-    // });
+    for (let i in car_brand_no) {
+      url =
+        "http://auto.danawa.com/newcar/?Work=search&Brand=" + car_brand_no[i];
+      await page.goto(url);
+      await page.waitForSelector(
+        "#autodanawa_gridC > div.gridWide > div.newcar_search > div.container_newcarlist > div.contents"
+      );
+      content = await page.content();
+      $ = cheerio.load(content);
+      lists = $(".list.modelList > li");
 
+      let car_name, model_no;
 
-    // for 문 필요합니다. car_brand_no[0] -> index
-    url = 'http://auto.danawa.com/newcar/?Work=search&Brand=' + car_brand_no[0];
-    await page.goto(url);
-    console.log(`Go to url : "${url}"`);
+      // let car_name, model_no;
+      // 차 이름 가져오기
+      for (let j = 0; j < lists.length; j++) {
+        car_name = $(lists[j])
+          .find("div.info > div > div > a.name.sendGA")
+          .text();
+        car_name = car_name.replace(/\t/g, "");
+        car_name = car_name.replace(/\n/g, "");
 
-    // 차 리스트 load 대기
-    await page.waitForSelector('#autodanawa_gridC > div.gridWide > div.newcar_search > div.container_newcarlist > div.contents');
+        // 차 model number
+        model_no = $(lists[j])
+          .find("div.info > div > div > a.name.sendGA")
+          .attr("model");
 
-    // cheerio = jquery 방식 셀렉트 모듈
-    // car list 접근
-    content = await page.content();
-    $ = cheerio.load(content);
-    lists = $('.list.modelList > li');
+        let colorList, element, car_color_name;
 
-    let car_name, model_no;
-    // 차 이름 가져오기
-    lists.each((idx, list) => {
-      car_name = $(list).find('div.info > div > div > a.name.sendGA').text();
-      car_name = car_name.replace(/\t/g, "");
-      car_name = car_name.replace(/\n/g, "");
+        // 차 model number로 자세한 페이지 이동
+        url = `http://auto.danawa.com/auto/?Work=model&Model=${model_no}&attributeList=`;
+        // 페이지 로드 대기
+        await page.goto(url, {
+          waitUntil: "networkidle2",
+        });
 
-      // 차 model number 가져오기
-      model_no = $(list).find('div.info > div > div > a.name.sendGA').attr('model');
+        // color 리스트 접근
+        colorList = await page.$$(
+          "#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div "
+        );
+        // 차종별 color 추출
+        if (colorList.length > 0) {
+          // 색상 이름이 담긴 태그 접근
+          element = await page.$(
+            "#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div"
+          );
+          // 색상 이름 가져오기 태그의 색상 리스트 처리
+          car_color_name = await page.evaluate((el) => el.textContent, element);
+          car_color_name = car_color_name.replace(/ +/g, "");
+          car_color_name = car_color_name.replace(/^\s+/, "");
+          car_color_name = car_color_name.replace(/\s+$/, "");
+          let division = car_color_name.split(/\n/g);
 
-      // car_data에 집어 넣기 (python의 append 개념)
-      car_data.push({
-        'car_model_no': model_no,
-        'car_name': car_name,
-        'car_brand_no': car_brand_no[0],
-        'car_color': []
-      });
-      // console.log({ idx, car_name, model_no });
-    });
+          for (let k in division) {
+            // division[i].substr(0, 2);
+            if (division[k] == "") division.splice(k, 1);
+            if (car_color_name) {
+              if (division[k].length == 0) {
+                division.splice(k, 1);
+              }
+            }
+          }
+          // hover 값 제거 : 항상 0번째 원소
+          division.splice(0, 1);
 
-    let colorList, element, btn, car_color_name, car_color_cd;
-    // 여기 주석은 무시하세요.
+          let c_code = [];
 
-    // for (let i = 0; i < lists.length; i++) {
-    //   url = `http://auto.danawa.com/auto/?Work=model&Model=${car_data[i]['car_model_no']}&attributeList=`;
-    //   await page.goto(url, {
-    //     waitUntil: 'networkidle2',
-    //   });
-    //   console.log(`Go to url : "${url}"`);
+          for (let p in division) {
+            element_code = await page.$(
+              "#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(" +
+              String(parseInt(p) + 2) +
+              ")"
+            );
+            car_color_code = await page.evaluate((el) => {
+              return getComputedStyle(el).getPropertyValue("background-color");
+            }, element_code);
+            c_code.push(rgbToHex(car_color_code));
+          }
 
-    //   // #autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(2)
-
-    //   // #autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(3)
-
-    // }
-
-    // 차 model number로 자세한 페이지 이동
-    url = `http://auto.danawa.com/auto/?Work=model&Model=${car_data[0]['car_model_no']}&attributeList=`;
-    // 페이지 로드 대기
-    await page.goto(url, {
-      waitUntil: 'networkidle2',
-    });
-
-    // color 리스트 접근
-    colorList = await page.$$('#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button');
-    console.log(colorList.length);
-
-    if (colorList.length > 0) {
-      // loop 필요합니다. hover가 필요할 것 같아 직접 넣어서 했는데
-      // 유능하신 윤재님은 아까 html 로드해서 다 가져온다고 하셨으니...
-      // for (let i = 2; i < colorList.length; i++) {
-      // hover 효과 적용
-      await page.hover(`#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(2)`);
-
-      // 색상 이름이 담긴 태그 접근
-      element = await page.$('#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > div');
-
-      // 색상 이름 가져오기
-      car_color_name = await page.evaluate(el => el.textContent, element);
-
-      // 이 부분 무시하세요.
-      // element = document.querySelector('#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(2)');
-      // comStyles = window.getComputedStyle(element);
-      // car_color_cd = comStyles.getPropertyValue('background');
-
-      // 차 색상 태그 접근하여 색상 코드 가져오기
-      car_color_cd = await page.evaluate(() => {
-        btn = document.querySelector(`#autodanawa_gridC > div.gridMain > article > main > div.modelSummary.auto.new > div.photo > div > button:nth-child(2)`);
-        return getComputedStyle(btn).getPropertyValue('background-color');
-      });
-      // car_color_cd = compStyles.getPropertyValue('background');
-
-      // 최상단에 rgbToHex 함수가 있습니다. 적용안하면 rgb (), (), () 형태로 나와서 변환 필요.
-      console.log(car_color_name, rgbToHex(car_color_cd));
-      // }
+          car_data = {
+            car_model_no: model_no,
+            car_name: car_name,
+            car_brand_no: car_brand_no[i],
+            car_color: division,
+            car_color_code: c_code,
+          };
+          console.log("===================");
+          console.log(car_data);
+        }
+      }
     }
-
     browser.close();
-    // 최종 car_data 찍어보기
-    // console.log(car_data);
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
   }
 })();
